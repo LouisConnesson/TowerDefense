31341063eb1be53621e359731290a24b55790965
LouisConnesson <connesson@et.esiea.fr>
2022-10-04 13:27:30 +0200

    update AR part

--- Assets/Scripts/ARCursor.cs
+++ Assets/Scripts/ARCursor.cs
@@ -8,20 +8,21 @@ public class ARCursor : MonoBehaviour
     public GameObject cursorChildObject;
     public GameObject objectToPlace;
     public ARRaycastManager raycastManager;
+
     public bool useCursor = true;
-    // Start is called before the first frame update
+
     void Start()
     {
         cursorChildObject.SetActive(useCursor);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (useCursor)
         {
-            updateCursor();
+            UpdateCursor();
         }
+
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             if (useCursor)
@@ -39,12 +40,14 @@ public class ARCursor : MonoBehaviour
             }
         }
     }
-    void updateCursor()
+
+    void UpdateCursor()
     {
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-        if(hits.Count > 0)
+
+        if (hits.Count > 0)
         {
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
