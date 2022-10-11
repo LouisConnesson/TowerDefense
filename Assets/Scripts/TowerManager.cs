7931df652fe0704c281ba9cd0fa4ff90a4edfa8d
Leboullenger <luisleboullenger@gmail.com>
2022-10-11 14:04:42 +0200

    Terrain Modifications

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
-using UnityEditor.VFX;
-using UnityEngine.Experimental.VFX;
 
 public class TowerManager : MonoBehaviour
 {
