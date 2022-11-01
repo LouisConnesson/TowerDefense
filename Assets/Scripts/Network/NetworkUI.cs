3c13f3189c514fa85b8ea776a4a83737fae33141
LuisLeb <luisleboullenger@gmail.com>
2022-11-01 14:44:54 +0100

    Added an UI field to enter host IP Adress

--- Assets/Scripts/Network/NetworkUI.cs
+++ Assets/Scripts/Network/NetworkUI.cs
@@ -1,43 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UI;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
 
 public class NetworkUI : MonoBehaviour
 {
+	private bool pcAssigned;
 
-    /*[SerializeField] private Button serverBtn;
-    [SerializeField] private Button hostBtn;
-    [SerializeField] private Button clientBtn;
+	[SerializeField] TextMeshProUGUI ipAddressText;
+	[SerializeField] TMP_InputField ip;
 
-    private void Awake()
-    {
-        serverBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartServer();
-        });
+	[SerializeField] string ipAddress;
+	[SerializeField] UnityTransport transport;
 
-        hostBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartHost();
-        });
+	void Start()
+	{
+		ipAddress = "0.0.0.0";
+		SetIpAddress(); // Set the Ip to the above address
+		pcAssigned = false;
+	}
 
-        clientBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartClient();
-        });
-    }*/
-    public void startServer()
-    {
-        NetworkManager.Singleton.StartServer();
-    }
-    public void startHost()
-    {
-        NetworkManager.Singleton.StartHost();
-    }
-    public void startClient()
-    {
-        NetworkManager.Singleton.StartClient();
-    }
+	// To Host a game
+	public void StartHost()
+	{
+		NetworkManager.Singleton.StartHost();
+		GetLocalIPAddress();
+	}
+
+	// To Join a game
+	public void StartClient()
+	{
+		ipAddress = ip.text;
+		SetIpAddress();
+		NetworkManager.Singleton.StartClient();
+	}
+
+	/* Gets the Ip Address of your connected network and
+	shows on the screen in order to let other players join
+	by inputing that Ip in the input field */
+	// ONLY FOR HOST SIDE 
+	public string GetLocalIPAddress()
+	{
+		var host = Dns.GetHostEntry(Dns.GetHostName());
+		foreach (var ip in host.AddressList)
+		{
+			if (ip.AddressFamily == AddressFamily.InterNetwork)
+			{
+				ipAddressText.text = ip.ToString();
+				ipAddress = ip.ToString();
+				return ip.ToString();
+			}
+		}
+		throw new System.Exception("No network adapters with an IPv4 address in the system!");
+	}
+
+	/* Sets the Ip Address of the Connection Data in Unity Transport
+	to the Ip Address which was input in the Input Field */
+	// ONLY FOR CLIENT SIDE
+	public void SetIpAddress()
+	{
+		transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+		transport.ConnectionData.Address = ipAddress;
+	}
 }
