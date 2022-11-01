e4b84ae5c67084e5383619f133a6092c2e800826
LuisLeb <luisleboullenger@gmail.com>
2022-11-01 12:09:43 +0100

    Created navMeshAgent mob that can navigate to castle spawned on server

--- Assets/Scripts/Network/PlayerNetwork.cs
+++ Assets/Scripts/Network/PlayerNetwork.cs
@@ -3,39 +3,40 @@ using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    [SerializeField] private Transform spawnObjectPrefab;
-    private Transform spawnedObjectTransform;
+    [SerializeField] private Transform mobPrefab; //change to list when their will be some differents
+    private List<Transform> mobList = new List<Transform>();
 
-    private NetworkVariable<MyCustomData> newValue = new NetworkVariable<MyCustomData>(
-        new MyCustomData { 
-            _int= 75,
-            _bool = false, 
-            _string = "nono",
-        }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    /*    private NetworkVariable<MyCustomData> newValue = new NetworkVariable<MyCustomData>(
+            new MyCustomData { 
+                _int= 75,
+                _bool = false, 
+                _string = "nono",
+            }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
-    public struct MyCustomData : INetworkSerializable
-    {
-        public int _int;
-        public bool _bool;
-        public FixedString128Bytes _string;
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        public struct MyCustomData : INetworkSerializable
         {
-            serializer.SerializeValue(ref _int);
-            serializer.SerializeValue(ref _bool);
-            serializer.SerializeValue(ref _string);
-        }
-    }
+            public int _int;
+            public bool _bool;
+            public FixedString128Bytes _string;
 
-    public override void OnNetworkSpawn()
-    {
-        newValue.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
-            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; "+ newValue._string);
-        };
-    }
+            public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+            {
+                serializer.SerializeValue(ref _int);
+                serializer.SerializeValue(ref _bool);
+                serializer.SerializeValue(ref _string);
+            }
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            newValue.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+                Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; "+ newValue._string);
+            };
+        }*/
 
     void Update()
     {
@@ -43,7 +44,7 @@ public class PlayerNetwork : NetworkBehaviour
 
         if (Input.GetKeyDown(KeyCode.T))
         {
-            SpawnMobsServerRPC(new ServerRpcParams());
+            SpawnMobsServerRPC(GetRandomSpawnPoint(GameManager.Instance.GetSpawnPointList()));
             //TestClientRPC(new ClientRpcParams { Send= new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } });
             /*            newValue.Value = new MyCustomData
                         {
@@ -55,7 +56,8 @@ public class PlayerNetwork : NetworkBehaviour
 
         if (Input.GetKeyDown(KeyCode.Y))
         {
-            Destroy(spawnObjectPrefab);
+            if (mobList[0])
+                Destroy(mobList[0]);
         }
 
         Vector3 InputVector = new Vector3(0, 0, 0);
@@ -82,13 +84,32 @@ public class PlayerNetwork : NetworkBehaviour
     }
 
     [ServerRpc]
-    public void SpawnMobsServerRPC(ServerRpcParams serverRpcParams)
+    public void SpawnMobsServerRPC(Vector3 spawnPosition)
     {
-        Debug.Log("TestServerRPC : " + OwnerClientId + " ; " + serverRpcParams.Receive.SenderClientId);
-        spawnedObjectTransform = Instantiate(spawnObjectPrefab);
+        //Debug.Log("TestServerRPC : " + OwnerClientId + " ; " + serverRpcParams.Receive.SenderClientId);
+
+        //SPAWN MOB AND INIT HIS INITIAL POSITION
+        Transform spawnedObjectTransform = Instantiate(mobPrefab, spawnPosition, Quaternion.identity, transform.parent);
         spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
 
-        spawnedObjectTransform.position = GameManager.Instance.GetRandomSpawnPoint();
+        //INIT HIS DESTINATION
+        //spawnedObjectTransform.position = spawnPosition;
+        SetDestinationToPosition(spawnedObjectTransform, GameManager.Instance.GetCastlePosition());
+
+        mobList.Add(spawnedObjectTransform);
+    }
+
+    public Vector3 GetRandomSpawnPoint(List<Transform> spawnPoints)
+    {
+        int rand = Random.Range(0, 3);
+        return spawnPoints[rand].position;
+    }
+
+    public void SetDestinationToPosition(Transform mob, Vector3 destination)
+    {
+        NavMeshAgent mobAgent = mob.GetComponent<NavMeshAgent>();
+        if (mobAgent != null)
+            mobAgent.SetDestination(destination);
     }
 
     [ClientRpc]
