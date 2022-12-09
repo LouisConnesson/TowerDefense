78d65f3f6079dc385812a1153a43849df71eec9d
Théo Lefeuvre <theo.lefeuvre@hotmail.fr>
2022-12-09 15:27:41 +0100

    mobs life + magic damages

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -3,24 +3,49 @@ using UnityEngine;
 
 public class FireBall : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float damages = 20;
     [SerializeField] private ParticleSystem fireBallParticles;
     [SerializeField] private ParticleSystem explosionParticles;
+    public Collider explosionCollider;
     void Start()
     {
         fireBallParticles.Play();
         explosionParticles.Stop();
+        if(explosionCollider)
+        explosionCollider.enabled = false;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Toggle the Collider on and off when pressing the space bar
+            explosionCollider.enabled = !explosionCollider.enabled;
+
+            //Output to console whether the Collider is on or not
+            Debug.Log("Collider.enabled = " + explosionCollider.enabled);
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (!IsOwner) return;
+        //if (!IsOwner) return;
         fireBallParticles.Stop();
+        if (explosionCollider)
+            explosionCollider.enabled = true;
+
         explosionParticles.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "mob")
+        {
+            Debug.Log("mob detected");
+
+            other.gameObject.GetComponent<MobLife>().TakeDamage(damages);
+        }
     }
 }
